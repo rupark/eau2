b04c32a344807e2305d3b472c8f9d31b8b63e3df
rupark <rupar.k@husky.neu.edu>
2020-03-30 01:25:48 -0400

    changes

--- src/network/network.h
+++ src/network/network.h
@@ -67,17 +67,19 @@ public:
        //cout << nodes_[0].address.sin_addr.s_addr << endl;
        for (size_t i =1; i < 3; i++) {
            Register* msg = dynamic_cast<Register*>(recv_m());
-           nodes_[msg->sender_].id = msg->sender_;
-           nodes_[msg->sender_].address.sin_family = AF_INET;
+//           nodes_[msg->sender_].id = msg->sender_;
+//           nodes_[msg->sender_].address.sin_family = AF_INET;
+           nodes_[i].id = i;
+           nodes_[i].address.sin_family = AF_INET;
            //nodes_[msg->sender_].address.sin_addr = msg->client.sin_addr;
            cout << "sender:" << msg->sender_ << endl;
-           if (i == 1 && inet_pton(AF_INET, "127.0.0.2", &nodes_[msg->sender_].address.sin_addr) <= 0) {
+           if (i == 1 && inet_pton(AF_INET, "127.0.0.2", &nodes_[i].address.sin_addr) <= 0) {
                assert(false && "Invalid server IP address format");
            }
-           if (i == 2 && inet_pton(AF_INET, "127.0.0.3", &nodes_[msg->sender_].address.sin_addr) <= 0) {
+           if (i == 2 && inet_pton(AF_INET, "127.0.0.3", &nodes_[i].address.sin_addr) <= 0) {
                assert(false && "Invalid server IP address format");
            }
-           nodes_[msg->sender_].address.sin_port = htons(msg->port);
+           nodes_[i].address.sin_port = htons(msg->port);
        }
        cout << "finished for loop nodes" << endl;
        size_t* ports = new size_t[2];
