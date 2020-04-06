91095cfd1414323a49e880ee33a6bbee51e63345
rupark <rupar.k@husky.neu.edu>
2020-04-06 09:40:40 -0400

    comments

--- src/writer.h
+++ src/writer.h
@@ -10,6 +10,7 @@
 #pragma once
 #include "row.h"
 #include "args.h"
+#include "SImap.h"
 
 class Writer {
 public:
@@ -105,4 +106,49 @@ public:
     FILE * file_;
 };
 
+class Summer : public Writer {
+public:
+    SIMap& map_;
+    size_t i = 0;
+    size_t j = 0;
+    size_t seen = 0;
 
+    Summer(SIMap& map) : map_(map) {}
+
+    void next() {
+        if (i == map_.capacity_ ) return;
+        if ( j < map_.items_[i].keys_.size() ) {
+            j++;
+            ++seen;
+        } else {
+            ++i;
+            j = 0;
+            while( i < map_.capacity_ && map_.items_[i].keys_.size() == 0 )  i++;
+            if (k()) ++seen;
+        }
+    }
+
+    String* k() {
+        if (i==map_.capacity_ || j == map_.items_[i].keys_.size()) return nullptr;
+        return (String*) (map_.items_[i].keys_.get_(j));
+    }
+
+    size_t v() {
+        if (i == map_.capacity_ || j == map_.items_[i].keys_.size()) {
+            assert(false); return 0;
+        }
+        return ((Num*)(map_.items_[i].vals_.get_(j)))->v;
+    }
+
+    void visit(Row& r) override {
+        cout << "visiting" <<endl;
+        if (!k()) next();
+        String* key = k();
+        size_t value = v();
+        r.set(0, key);
+        r.set(1, (int) value);
+        next();
+    }
+
+    bool done() override {return seen == map_.size(); }
+};
