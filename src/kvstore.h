8fd8a857c2320b6f12a4f93ded7c7ce1885f4a09
rupark <rupar.k@husky.neu.edu>
2020-04-06 13:58:45 -0400

    comments

--- src/kvstore.h
+++ src/kvstore.h
@@ -47,7 +47,6 @@ public:
         for (int i = 0; i < size; i++) {
             if (key.equals(keys[i])) {
                 cout << "key found: " << i << endl;
-                cout << dfs[i]->ncol << endl;
                 if (dfs[i] != nullptr) {
                     return dfs[i];
                 } else {
