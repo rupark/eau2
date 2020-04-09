053f5576230363519867fc8b512317e3f0a1e1a6
rupark <rupar.k@husky.neu.edu>
2020-04-09 19:48:52 -0400

    comments

--- src/writer.h
+++ src/writer.h
@@ -12,6 +12,7 @@
 #include "row.h"
 #include "args.h"
 #include "SImap.h"
+#include "set.h"
 
 class Writer {
 public:
@@ -190,3 +191,25 @@ public:
     /** Returns true when there are no more words String, Num pairs in SIMap */
     bool done() override { return seen >= map_.size(); }
 };
+
+/*****************************************************************************
+ * A SetWriter copies all the values present in the set into a one-column
+ * dataframe. The data contains all the values in the set. The dataframe has
+ * at least one integer column.
+ ****************************************************************************/
+class SetWriter: public Writer {
+public:
+    Set& set_; // set to read from
+    int i_ = 0;  // position in set
+
+    SetWriter(Set& set): set_(set) { }
+
+    /** Skip over false values and stop when the entire set has been seen */
+    bool done() {
+        while (i_ < set_.size_ && set_.test(i_) == false) ++i_;
+        return i_ == set_.size_;
+    }
+
+    void visit(Row & row) { row.set(0, i_++); }
+};
+
