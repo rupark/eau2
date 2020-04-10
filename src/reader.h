6f8b54d9d61c2928d021b488b6f46206cd1bbbc8
rupark <rupar.k@husky.neu.edu>
2020-04-09 20:25:45 -0400

    comments

--- src/reader.h
+++ src/reader.h
@@ -6,7 +6,7 @@
 
 #include "row.h"
 #include "SImap.h"
-#include "set.h"
+//#include "set.h"
 
 class Reader {
 public:
@@ -37,83 +37,83 @@ public:
         return false;
     }
 };
-
-/*******************************************************************************
- * A SetUpdater is a reader that gets the first column of the data frame and
- * sets the corresponding value in the given set.
- ******************************************************************************/
-class SetUpdater : public Reader {
-public:
-    Set& set_; // set to update
-
-    SetUpdater(Set& set): set_(set) {}
-
-    /** Assume a row with at least one column of type I. Assumes that there
-     * are no missing. Reads the value and sets the corresponding position.
-     * The return value is irrelevant here. */
-    bool visit(Row & row) { set_.set(row.get_int(0));  return false; }
-
-};
-
-/***************************************************************************
- * The ProjectTagger is a reader that is mapped over commits, and marks all
- * of the projects to which a collaborator of Linus committed as an author.
- * The commit dataframe has the form:
- *    pid x uid x uid
- * where the pid is the identifier of a project and the uids are the
- * identifiers of the author and committer. If the author is a collaborator
- * of Linus, then the project is added to the set. If the project was
- * already tagged then it is not added to the set of newProjects.
- *************************************************************************/
-class ProjectsTagger : public Reader {
-public:
-    Set& uSet; // set of collaborator
-    Set& pSet; // set of projects of collaborators
-    Set newProjects;  // newly tagged collaborator projects
-
-    ProjectsTagger(Set& uSet, Set& pSet, DataFrame* proj):
-            uSet(uSet), pSet(pSet), newProjects(proj) {}
-
-    /** The data frame must have at least two integer columns. The newProject
-     * set keeps track of projects that were newly tagged (they will have to
-     * be communicated to other nodes). */
-    bool visit(Row & row) override {
-        int pid = row.get_int(0);
-        int uid = row.get_int(1);
-        if (uSet.test(uid))
-            if (!pSet.test(pid)) {
-                pSet.set(pid);
-                newProjects.set(pid);
-            }
-        return false;
-    }
-};
-
-/***************************************************************************
- * The UserTagger is a reader that is mapped over commits, and marks all of
- * the users which commmitted to a project to which a collaborator of Linus
- * also committed as an author. The commit dataframe has the form:
- *    pid x uid x uid
- * where the pid is the idefntifier of a project and the uids are the
- * identifiers of the author and committer.
- *************************************************************************/
-class UsersTagger : public Reader {
-public:
-    Set& pSet;
-    Set& uSet;
-    Set newUsers;
-
-    UsersTagger(Set& pSet,Set& uSet, DataFrame* users):
-            pSet(pSet), uSet(uSet), newUsers(users->nrows()) { }
-
-    bool visit(Row & row) override {
-        int pid = row.get_int(0);
-        int uid = row.get_int(1);
-        if (pSet.test(pid))
-            if(!uSet.test(uid)) {
-                uSet.set(uid);
-                newUsers.set(uid);
-            }
-        return false;
-    }
-};
+//
+///*******************************************************************************
+// * A SetUpdater is a reader that gets the first column of the data frame and
+// * sets the corresponding value in the given set.
+// ******************************************************************************/
+//class SetUpdater : public Reader {
+//public:
+//    Set& set_; // set to update
+//
+//    SetUpdater(Set& set): set_(set) {}
+//
+//    /** Assume a row with at least one column of type I. Assumes that there
+//     * are no missing. Reads the value and sets the corresponding position.
+//     * The return value is irrelevant here. */
+//    bool visit(Row & row) { set_.set(row.get_int(0));  return false; }
+//
+//};
+//
+///***************************************************************************
+// * The ProjectTagger is a reader that is mapped over commits, and marks all
+// * of the projects to which a collaborator of Linus committed as an author.
+// * The commit dataframe has the form:
+// *    pid x uid x uid
+// * where the pid is the identifier of a project and the uids are the
+// * identifiers of the author and committer. If the author is a collaborator
+// * of Linus, then the project is added to the set. If the project was
+// * already tagged then it is not added to the set of newProjects.
+// *************************************************************************/
+//class ProjectsTagger : public Reader {
+//public:
+//    Set& uSet; // set of collaborator
+//    Set& pSet; // set of projects of collaborators
+//    Set newProjects;  // newly tagged collaborator projects
+//
+//    ProjectsTagger(Set& uSet, Set& pSet, DataFrame* proj):
+//            uSet(uSet), pSet(pSet), newProjects(proj) {}
+//
+//    /** The data frame must have at least two integer columns. The newProject
+//     * set keeps track of projects that were newly tagged (they will have to
+//     * be communicated to other nodes). */
+//    bool visit(Row & row) override {
+//        int pid = row.get_int(0);
+//        int uid = row.get_int(1);
+//        if (uSet.test(uid))
+//            if (!pSet.test(pid)) {
+//                pSet.set(pid);
+//                newProjects.set(pid);
+//            }
+//        return false;
+//    }
+//};
+//
+///***************************************************************************
+// * The UserTagger is a reader that is mapped over commits, and marks all of
+// * the users which commmitted to a project to which a collaborator of Linus
+// * also committed as an author. The commit dataframe has the form:
+// *    pid x uid x uid
+// * where the pid is the idefntifier of a project and the uids are the
+// * identifiers of the author and committer.
+// *************************************************************************/
+//class UsersTagger : public Reader {
+//public:
+//    Set& pSet;
+//    Set& uSet;
+//    Set newUsers;
+//
+//    UsersTagger(Set& pSet,Set& uSet, DataFrame* users):
+//            pSet(pSet), uSet(uSet), newUsers(users->nrows()) { }
+//
+//    bool visit(Row & row) override {
+//        int pid = row.get_int(0);
+//        int uid = row.get_int(1);
+//        if (pSet.test(pid))
+//            if(!uSet.test(uid)) {
+//                uSet.set(uid);
+//                newUsers.set(uid);
+//            }
+//        return false;
+//    }
+//};
--- src/writer.h
+++ src/writer.h
@@ -12,7 +12,7 @@
 #include "row.h"
 #include "args.h"
 #include "SImap.h"
-#include "set.h"
+//#include "set.h"
 
 class Writer {
 public:
@@ -192,24 +192,24 @@ public:
     bool done() override { return seen >= map_.size(); }
 };
 
-/*****************************************************************************
- * A SetWriter copies all the values present in the set into a one-column
- * dataframe. The data contains all the values in the set. The dataframe has
- * at least one integer column.
- ****************************************************************************/
-class SetWriter: public Writer {
-public:
-    Set& set_; // set to read from
-    int i_ = 0;  // position in set
-
-    SetWriter(Set& set): set_(set) { }
-
-    /** Skip over false values and stop when the entire set has been seen */
-    bool done() {
-        while (i_ < set_.size_ && set_.test(i_) == false) ++i_;
-        return i_ == set_.size_;
-    }
-
-    void visit(Row & row) { row.set(0, i_++); }
-};
+///*****************************************************************************
+// * A SetWriter copies all the values present in the set into a one-column
+// * dataframe. The data contains all the values in the set. The dataframe has
+// * at least one integer column.
+// ****************************************************************************/
+//class SetWriter: public Writer {
+//public:
+//    Set& set_; // set to read from
+//    int i_ = 0;  // position in set
+//
+//    SetWriter(Set& set): set_(set) { }
+//
+//    /** Skip over false values and stop when the entire set has been seen */
+//    bool done() {
+//        while (i_ < set_.size_ && set_.test(i_) == false) ++i_;
+//        return i_ == set_.size_;
+//    }
+//
+//    void visit(Row & row) { row.set(0, i_++); }
+//};
 
