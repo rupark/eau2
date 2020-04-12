bb3d97f98f9b91d43b4dad9bb58a569056012727
rupark <rupar.k@husky.neu.edu>
2020-04-12 18:51:37 -0400

    debug

--- src/network/wordcount.h
+++ src/network/wordcount.h
@@ -147,7 +147,7 @@ public:
             Status *ipd = dynamic_cast<Status *>(this->net.recv_m()); // Put this in Kv?
             cout << "client received" << endl;
 
-            DataFrame* chunkSoFar = kv.get(in);
+            DataFrame* chunkSoFar = kv.get(*new Key("data"));
             chunkSoFar->append_chunk(ipd->msg_);
             kv.put(new Key("data"),chunkSoFar); // check if put is needed? df pointer manipulated...
 
