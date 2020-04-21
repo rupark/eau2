852c95a74fe5a23bcb633182c663b0c34665881f
rupark <rupar.k@husky.neu.edu>
2020-04-21 14:20:36 -0400

    updates

--- tests/m4/main.cpp
+++ tests/m4/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
     NetworkIP *network = initialize();
     assert(arg.num_nodes != 0 && "cannot have empty cloud");
 //    try {
-        Application *app = pick(network->index(), *network);
+        Linus *app = new Linus(network->index(), *network);
         cout << "CHOSEN APP: " << arg.app << endl;
         app->run_();
         cout << "Finished Running App" << endl;
