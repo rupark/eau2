6290275a64fb12d3a5ec50715d2b1292c979a003
rupark <rupar.k@husky.neu.edu>
2020-04-21 13:19:30 -0400

    updates

--- src/network/linus.h
+++ src/network/linus.h
@@ -36,8 +36,8 @@ public:
     Linus(size_t idx, NetworkIP& net): Application(idx, net) {}
 
     ~Linus() {
-        delete[] PROJ;
-        delete[] USER;
+//        delete[] PROJ;
+//        delete[] USER;
 //        delete[] COMM;
 //        delete projects;
 //        delete users;
