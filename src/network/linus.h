d8a840a21391df0578b6915f79c79de2d854baf8
rupark <rupar.k@husky.neu.edu>
2020-04-22 15:22:32 -0400

    400 blocks of mem leak, segfault after end of exec

--- src/network/linus.h
+++ src/network/linus.h
@@ -248,7 +248,7 @@ public:
             // Split into chunks and send iteratively to nodes
             int selectedNode = 0;
 
-            for (size_t j = 0; j < num_chunks; j++) {
+            for (size_t j = 1; j < num_chunks; j++) {
                 DataFrame *cur_chunk = newUsers->chunk(j);
                 // if server's turn, keep chunks of DataFrame.
                 if (selectedNode == 0) {
