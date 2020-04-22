55ecbe38d0b888edd555542bb3873035103e52e1
rupark <rupar.k@husky.neu.edu>
2020-04-22 19:07:08 -0400

    400 blocks of mem leak, segfault after end of exec

--- src/network/wordcount.h
+++ src/network/wordcount.h
@@ -87,7 +87,7 @@ public:
 
     WordCount(size_t idx, NetworkIP &net) :
             Application(idx, net), in("data"), kbuf(new Key("wc-map-", 0)), words_all("words-all") {
-        kv.put(&in, new DataFrame(*new Schema("S")));
+        kv->put(&in, new DataFrame(*new Schema("S")));
     }
 
     /** The master nodes reads the input, then all of the nodes count. */
@@ -99,7 +99,7 @@ public:
             DataFrame *df = fromVisitor(&words_all, &kv, "S", fr);
 
             // Split into chunks and send iteratively to nodes
-            int num_chunks = 1 + ((df->nrow - 1) / arg.rows_per_chunk);
+            int num_chunks = 1 + ((df->get_num_rows() - 1) / arg.rows_per_chunk);
             int selectedNode = 0;
 
             for (size_t j = 0; j < num_chunks; j++) {
@@ -108,9 +108,9 @@ public:
                 // if server's turn, keep chunks of DataFrame.
                 if (selectedNode == 0) {
                     // put chunks into local kv store as received
-                    DataFrame *chunkSoFar = kv.get(*new Key("data"));
+                    DataFrame *chunkSoFar = kv->get(*new Key("data"));
                     chunkSoFar->append_chunk(cur_chunk);
-                    kv.put(new Key("data"), chunkSoFar);
+                    kv->put(new Key("data"), chunkSoFar);
                 } else {
                     // Sending to Clients
                     Status *chunkMsg = new Status(0, selectedNode, cur_chunk);
@@ -130,7 +130,7 @@ public:
                 StrBuff *s = new StrBuff();
                 s->c("wc-map-");
                 s->c(msg->sender_);
-                this->kv.put(new Key(s->get()), msg->msg_);
+                this->kv->put(new Key(s->get()), msg->msg_);
             }
 
             //Theoretically everyone should now be in the store to reduce
@@ -143,7 +143,7 @@ public:
             DataFrame *df = fromVisitor(&words_all, &kv, "S", fr);
 
             //Calculating the number of chunks and figuring out how many go to this node
-            int num_chunks = 1 + ((df->nrow - 1) / arg.rows_per_chunk);
+            int num_chunks = 1 + ((df->get_num_rows() - 1) / arg.rows_per_chunk);
             int num_received = 0;
             int selectedNode = 0;
             for (int i = 0; i < num_chunks; i++) {
@@ -161,9 +161,9 @@ public:
                 Status *ipd = dynamic_cast<Status *>(this->net.recv_m());
                 cout << "client received" << endl;
 
-                DataFrame *chunkSoFar = kv.get(*new Key("data"));
+                DataFrame *chunkSoFar = kv->get(*new Key("data"));
                 chunkSoFar->append_chunk(ipd->msg_);
-                kv.put(new Key("data"), chunkSoFar);
+                kv->put(new Key("data"), chunkSoFar);
             }
 
             local_count();
@@ -171,7 +171,7 @@ public:
             s->c("wc-map-");
             s->c(this->idx_);
             Key *key_counts2 = new Key(s->get());
-            DataFrame *storeDF = kv.get(key_counts2);
+            DataFrame *storeDF = kv->get(key_counts2);
             Status msg(this->idx_, 0, storeDF);
             this->net.send_m(&msg);
             cout << "sending chunk back" << endl;
@@ -208,7 +208,7 @@ public:
 
     /** Compute word counts on the local node and build a data frame. */
     void local_count() {
-        DataFrame *words = kv.get(in);
+        DataFrame *words = kv->get(in);
         SIMap map;
         Adder add(map);
         words->local_map(add);
@@ -232,7 +232,7 @@ public:
         s->c("wc-map-");
         s->c(this->idx_);
         Key *own = new Key(s->get());
-        DataFrame *df = kv.get(*own);
+        DataFrame *df = kv->get(*own);
         merge(df, map);
 
         for (size_t i = 1; i < arg.num_nodes; ++i) { // merge other nodes
@@ -241,7 +241,7 @@ public:
             s->c(i);
 
             Key *ok = new Key(s->get());
-            merge(kv.get(*ok), map);
+            merge(kv->get(*ok), map);
         }
 
         cout << "Different words: " << map.size() << endl;
