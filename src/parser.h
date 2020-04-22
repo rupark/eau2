43281ef3af41ede2bee2886918961bc454acecca
rupark <rupar.k@husky.neu.edu>
2020-04-22 11:10:27 -0400

    THIS VERSION DOESNT SEGFAULT

--- src/parser.h
+++ src/parser.h
@@ -86,8 +86,8 @@ public:
     virtual char *toCString() {
         size_t length = _end - _start;
         char *sliceCopy = new char[length + 1];
-        delete[] sliceCopy;
         memcpy(sliceCopy, getChars(), length);
+        delete sliceCopy[length];
         sliceCopy[length] = '\0';
         return sliceCopy;
     }
