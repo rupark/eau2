40567e04a9b69a8131bccbf127d71e3ad78f5244
rupark <rupar.k@husky.neu.edu>
2020-04-22 21:32:40 -0400

    Report

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -218,9 +218,8 @@ void testKV() {
     for (size_t i = 0; i < SZ; ++i) sum += vals[i] = i;
     Key key("triv",0);
     KVStore* kv = new KVStore();
-    DataFrame* df = Linus::fromArray(&key, kv, SZ, vals);
-    assert(df->get_double(0,1) == 1);
-    DataFrame* df2 = kv.get(key);
+    Linus::fromArray(&key, kv, SZ, vals);
+    DataFrame* df2 = kv->get(key);
     for (size_t i = 0; i < SZ; ++i) sum -= df2->get_double(0,i);
     assert(sum==0);
 }
