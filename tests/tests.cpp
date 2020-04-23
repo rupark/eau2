60f4e5b3ee8f666cbfd1b7c123eb898b4470e2d8
rupark <rupar.k@husky.neu.edu>
2020-04-22 21:22:55 -0400

    Report

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -124,59 +124,47 @@ void test_strSlice() {
 
 void test_serialization() {
     DataFrame* d = new DataFrame(*new Schema("BFIS"));
-    cout << "DF Created" << endl;
     d->columns[0]->push_back((bool)1);
-    cout << "Push 0" << endl;
     d->columns[1]->push_back((float)3.0);
-    cout << "Push 1" << endl;
     d->columns[2]->push_back((int)3);
-    cout << "Push 2" << endl;
     d->columns[3]->push_back(new String("h"));
-    cout << "Push 3" << endl;
     d->columns[0]->push_back((bool)0);
-    cout << "Push 4" << endl;
     d->columns[1]->push_back((float)4.0);
-    cout << "Push 5" << endl;
     d->columns[2]->push_back((int)6);
-    cout << "Push 6" << endl;
     d->columns[3]->push_back(new String("f"));
-    cout << "Push 7" << endl;
     Status* s = new Status(0, 0, d);
-    cout << "STATUS CREATED" << endl;
     char* serialized = s->serialize()->cstr_;
-    cout << serialized << endl;
+    assert(strcmp(serialized, "3?0?0?0?B}1}0}!F}3.000000}4.000000}!I}3}6}!S}h}f}!") == 0);
 
     Status* s2 = new Status(serialized);
-    cout << s2->sender_ << endl;
-    cout << s2->target_ << endl;
-    cout << s2->id_ << endl;
-    for (int i = 0; i < s2->msg_->get_num_cols(); i++) {
-        for (int j = 0; j < s2->msg_->columns[i]->size(); j++) {
-            switch (s2->msg_->columns[i]->get_type()) {
-                case 'F':
-                    cout << *s2->msg_->columns[i]->as_float()->get(j);
-                    break;
-                case 'S':
-                    cout << *s2->msg_->columns[i]->as_string()->get(j)->cstr_;
-                    break;
-                case 'B':
-                    cout << *s2->msg_->columns[i]->as_bool()->get(j);
-                    break;
-                case 'I':
-                    cout << *s2->msg_->columns[i]->as_int()->get(j);
-                    break;
-            }
-        }
-    }
+    assert(0 == s2->sender_);
+    assert(0 == s2->target_);
+    assert(0 == s2->id_);
+//    for (int i = 0; i < s2->msg_->get_num_cols(); i++) {
+//        for (int j = 0; j < s2->msg_->columns[i]->size(); j++) {
+//            switch (s2->msg_->columns[i]->get_type()) {
+//                case 'F':
+//                    cout << *s2->msg_->columns[i]->as_float()->get(j);
+//                    break;
+//                case 'S':
+//                    cout << *s2->msg_->columns[i]->as_string()->get(j)->cstr_;
+//                    break;
+//                case 'B':
+//                    cout << *s2->msg_->columns[i]->as_bool()->get(j);
+//                    break;
+//                case 'I':
+//                    cout << *s2->msg_->columns[i]->as_int()->get(j);
+//                    break;
+//            }
+//        }
+//    }
 }
 
 void serial2() {
 
     Ack* a = new Ack(0, 1);
     Ack* g = new Ack(a->serialize()->cstr_);
-    if (strcmp(a->serialize()->cstr_,g->serialize()->cstr_) == 0) {
-        cout << "Ack OK" << endl;
-    }
+    assert(strcmp(a->serialize()->cstr_,g->serialize()->cstr_) == 0);
 
     size_t* ports = new size_t[3];
     ports[0] = 1;
@@ -188,9 +176,7 @@ void serial2() {
     add[2] = new String("127.0.0.3");
     Directory* d = new Directory(ports, add, 2);
     Directory* c = new Directory(d->serialize()->cstr_);
-    if (strcmp(d->serialize()->cstr_,c->serialize()->cstr_) == 0) {
-        cout << "Directory OK" << endl;
-    }
+    assert(strcmp(d->serialize()->cstr_,c->serialize()->cstr_) == 0);
 }
 
 void testDf() {
